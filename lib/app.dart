d6d043bd21193da7b963250087e268c79051cefc
[abe-tk] <[abe.tk6@gmail.com]>
2025-04-19 01:37:05 +0900

    flutter_localizationsの対応

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'config/theme.dart';
@@ -11,6 +12,12 @@ class MyApp extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp.router(
       theme: lightTheme(),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [Locale('ja')],
       routerConfig: ref.watch(goRouterProvider),
     );
   }
