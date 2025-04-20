6dc99ec70d740c3e88bc234a157bce843be57bf4
[abe-tk] <[abe.tk6@gmail.com]>
2025-04-21 00:28:44 +0900

    タスク一覧アイテムの表示を調整

--- lib/presentation/page/todo/widget/todo_list_item.dart
+++ lib/presentation/page/todo/widget/todo_list_item.dart
@@ -5,7 +5,7 @@ import '../../../../feature/todo/application/usecase/todo_usecase.dart';
 import '../../../../feature/todo/domain/entity/todo_entity.dart';
 import '../../../../l10n/l10n.dart';
 import '../../../../routing/go_router.dart';
-import '../../../../util/formatter/date_time_formatter.dart';
+import '../../../../util/extension/date_time_formatter.dart';
 import '../../../common_widget/app_snack_bar.dart';
 import '../../../mixin/page_mixin.dart';
 
@@ -42,29 +42,35 @@ class TodoListItem extends ConsumerWidget with PageMixin {
     }
 
     return ListTile(
-      titleAlignment: ListTileTitleAlignment.top,
+      titleAlignment:
+          todo.description == null && todo.dueDate == null
+              ? null
+              : ListTileTitleAlignment.top,
       title: Text(todo.title),
-      subtitle: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          if (todo.description != null)
-            Text(
-              todo.description!,
-              style: textTheme.bodyMedium!.copyWith(
-                color: colorScheme.onSurfaceVariant,
+      subtitle:
+          todo.description == null && todo.dueDate == null
+              ? null
+              : Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  if (todo.description != null)
+                    Text(
+                      todo.description!,
+                      style: textTheme.bodyMedium!.copyWith(
+                        color: colorScheme.onSurfaceVariant,
+                      ),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  if (todo.dueDate != null)
+                    Text(
+                      todo.dueDate!.MMMEd(),
+                      style: textTheme.bodyMedium!.copyWith(
+                        color: colorScheme.onSurface,
+                      ),
+                    ),
+                ],
               ),
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-          if (todo.dueDate != null)
-            Text(
-              todo.dueDate!.MMMEd(),
-              style: textTheme.bodyMedium!.copyWith(
-                color: colorScheme.onSurface,
-              ),
-            ),
-        ],
-      ),
       leading: Checkbox(
         value: todo.isCompleted,
         onChanged: (value) {
